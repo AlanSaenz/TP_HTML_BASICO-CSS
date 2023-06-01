88cbc39fc5786200a2e0532d70478632e3928417
Alan Saenz <alansaenz95@gmail.com>
2023-05-31 21:39:32 -0300

    Actualizacion + Javascript

---
+++ script/script1-.js
@@ -0,0 +1,14 @@
+var nombre = prompt("¿Cuál es su nombre?");
+alert("Bienvenido, " + nombre + "!");
+
+var medio = prompt("¿Cómo llegó a la web ?");
+//alert("Llegué por " + medio + ".");
+
+var opinion1 = prompt("¿Fue fácil navegar por el sitio web (si/no)?");
+//alert("El sitio le fue útil, " + opinion1);
+
+var opinion2 = prompt("¿La información proporcionada en la página fue útil (si/no)?");
+//alert(opinion2);
+
+var opinion3 = prompt("¿Recomendaría que visiten esta web? (si/no)");
+//alert(opinion3);
